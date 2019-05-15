2ddbf850190c78a748a700fa311dad3e51d9141b
git-pusher <rachel.palomeque@gmail.com>
2019-05-15 15:53:57 -0500

    Updates

---
+++ precache-manifest.c6b3ee2d338220296a83613b43cb1d9f.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "03fa02aab28629fa449d567adff218eb",
+    "url": "/STAR-WARS/static/media/logo.03fa02aa.png"
+  },
+  {
+    "revision": "e2f11fbf7fb9719bb95996916fdc54ea",
+    "url": "/STAR-WARS/static/media/fondo.e2f11fbf.jpg"
+  },
+  {
+    "revision": "9413ebbeea93e97fc369",
+    "url": "/STAR-WARS/static/js/runtime~main.20753dcd.js"
+  },
+  {
+    "revision": "1a59086b3c5860349f51",
+    "url": "/STAR-WARS/static/js/main.04d582b4.chunk.js"
+  },
+  {
+    "revision": "2f8ff84f9a3ec93f8ebd",
+    "url": "/STAR-WARS/static/js/2.416bc6f4.chunk.js"
+  },
+  {
+    "revision": "1a59086b3c5860349f51",
+    "url": "/STAR-WARS/static/css/main.98c32f21.chunk.css"
+  },
+  {
+    "revision": "2fb2a58a1889a8946c74fca375548d7d",
+    "url": "/STAR-WARS/index.html"
+  }
+];
